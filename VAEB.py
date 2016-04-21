f32b3e9baa1d5dbc52d8ae9973215ac8215b5ca5
tom <tfwnicholson@gmail.com>
2016-04-21 16:07:55 +0100

    Add ability to write out traces

--- VAEB.py
+++ VAEB.py
@@ -313,3 +313,7 @@ if __name__ == '__main__':
         print("Epoch %s : [Lower bound: %s, time: %s]" % (epoch, LB, time.time() - start))
 
         print("          [Lower bound on validation set: %s]" % LBvalidation)
+
+        if len(trace_file) > 0 :
+            with open(trace_file, 'a') as f :
+                f.write('{0},{1},{2}\n'.format(model.N * (epoch + 1), LB, LBvalidation))
